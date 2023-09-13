c68c9825b6d389df10f028b2718e3400de39ef65
Rowland Adimoha <rowlives@gmail.com>
2023-09-13 12:15:12 +0100

    setting up working directory

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,13 @@
-// src/app.ts
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
-import dotenv from "dotenv";
-
-// Load environment variables from .env file
-dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
-app.use(express.json());
+// Define a basic route
+app.get("/api/v1/naturants", (req, res) => {
+  res.json({ message: "Welcome to Naturants API" });
+});
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI || "", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
-
-// Routes
-// Add your route definitions here
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
