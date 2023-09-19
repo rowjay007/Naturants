d25c8b9288261534425e17eabc01697a92ab52ce
Rowland Adimoha <rowlives@gmail.com>
2023-09-19 13:36:32 +0100

    running server with v1 data

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
 import express from "express";
+import { readFile } from "fs/promises";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = 3001;
 
-// Define a basic route
-app.get("/api/v1/naturants", (req, res) => {
-  res.json({ message: "Welcome to Naturants API" });
+app.use(express.json());
+
+const dataPath = "src/data/naturants-sample.json"; // Assuming your data file is in the src/data directory
+
+app.get("/api/v1/naturants", async (req, res) => {
+  try {
+    const rawData = await readFile(dataPath, "utf-8");
+    const naturants = JSON.parse(rawData);
+
+    const response = {
+      status: "success",
+      length: naturants.length,
+      data: naturants,
+    };
+
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running on port ${PORT}`);
 });
