0dc4d2b44dba600cb01c51636f9b4c79a2d57155
Rowland Adimoha <rowlives@gmail.com>
2023-09-20 09:58:43 +0100

    using router and url paramter to manage data

--- src/app.ts
+++ src/app.ts
@@ -8,23 +8,58 @@ app.use(express.json());
 
 const dataPath = "src/data/naturants-sample.json"; // Assuming your data file is in the src/data directory
 
-app.get("/api/v1/naturants", async (req, res) => {
+interface Naturant {
+  id: number;
+  // Add other properties here as needed
+}
+
+// Refactor the code to use an Express router
+const router = express.Router();
+
+// Define a route with a URL parameter
+router.get("/api/v1/naturants/:id", async (req, res) => {
   try {
+    const { id } = req.params;
     const rawData = await readFile(dataPath, "utf-8");
-    const naturants = JSON.parse(rawData);
+    const naturants: Naturant[] = JSON.parse(rawData); // Annotate the type here
+    const selectedNaturant = naturants.find(
+      (naturant) => naturant.id === parseInt(id)
+    );
 
-    const response = {
+    if (!selectedNaturant) {
+      res.status(404).json({ error: "Naturant not found" });
+      return;
+    }
+
+    res.json({
       status: "success",
-      length: naturants.length,
-      data: naturants,
-    };
-
-    res.json(response);
+      data: selectedNaturant,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+// Define a route to handle GET requests to /api/v1/naturants without an id parameter
+router.get("/api/v1/naturants", async (req, res) => {
+  try {
+    const rawData = await readFile(dataPath, "utf-8");
+    const naturants: Naturant[] = JSON.parse(rawData);
+
+    res.json({
+      status: "success",
+      length: naturants.length,
+      data: naturants,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Mount the router on the main Express app
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running on port ${PORT}`);
