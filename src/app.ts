317e681485b4b408c15517c79351fa84ade0caa9
Rowland Adimoha <rowlives@gmail.com>
2023-09-25 12:56:43 +0100

    updating both environment variables for both production and development

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from "cors";
+import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import naturantsRoutes from "./routes/naturantsRoutes";
 import usersRoutes from "./routes/usersRoutes";
 
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_PORT
+    : process.env.DEV_PORT;
 
 // Middleware to set common response headers
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -16,7 +22,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use(express.json());
-app.use(morgan("dev"));
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms - :date[clf]"
+  )
+);
+
 app.use(cors());
 
 app.use("/api/v1/users", usersRoutes);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
-import { app, port } from "./app";
+import dotenv from "dotenv";
+import { app } from "./app";
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+dotenv.config(); // Load environment variables from .env file
+
+const env = process.env.NODE_ENV || "development";
+const serverPort =
+  env === "production" ? process.env.PROD_PORT : process.env.DEV_PORT;
+
+app.listen(serverPort, () => {
+  console.log(`Server is listening on port ${serverPort}`);
 });
