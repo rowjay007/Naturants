64de4fed8ffcbb6d43e1602cceee10826642269c
Rowland Adimoha <rowlives@gmail.com>
2023-10-03 10:01:24 +0100

    edit: fix checkID from middleware

--- src/routes/naturantsRoutes.ts
+++ src/routes/naturantsRoutes.ts
@@ -8,15 +8,14 @@ import {
   updateNaturantPartially,
   parseNaturantId,
 } from "../controllers/naturantsController";
-import { checkID } from "../middleware/middleware";
 
 const router = express.Router();
 
 router.get("/", getAllNaturants);
 router.post("/", createNaturant);
-router.get("/:id", checkID, parseNaturantId, getNaturantById); // Use the middleware here
-router.put("/:id", checkID, parseNaturantId, updateNaturantById); // Use the middleware here
-router.patch("/:id", checkID, parseNaturantId, updateNaturantPartially); // Use the middleware here
-router.delete("/:id", checkID, parseNaturantId, deleteNaturantById); // Use the middleware here
+router.get("/:id", parseNaturantId, getNaturantById); // Remove checkID middleware
+router.put("/:id", parseNaturantId, updateNaturantById); // Remove checkID middleware
+router.patch("/:id", parseNaturantId, updateNaturantPartially); // Remove checkID middleware
+router.delete("/:id", parseNaturantId, deleteNaturantById); // Remove checkID middleware
 
 export default router;
