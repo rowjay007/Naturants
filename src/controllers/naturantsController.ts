da5765183a9753101973e6ede48a9e5317102017
Rowland Adimoha <rowlives@gmail.com>
2023-10-05 10:08:02 +0100

    creating a utility function for code refactor

--- src/controllers/naturantsController.ts
+++ src/controllers/naturantsController.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// controllers/naturantsController.ts
 import express from "express";
-import { SortOrder } from "mongoose";
 import NaturantsModel from "../models/naturantsModel";
+import { ApiFeatures } from "../utils/apiFeatures";
 
 export async function parseNaturantId(
   req: express.Request,
@@ -19,58 +19,14 @@ export async function getAllNaturants(
   next: express.NextFunction
 ) {
   try {
-    // Extract query parameters for filtering, sorting, field limiting, and pagination
-    const {
-      filterField,
-      filterValue,
-      sortField,
-      sortOrder,
-      fields,
-      page,
-      limit,
-    } = req.query;
+    const features = new ApiFeatures(NaturantsModel.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    // Build the filter options
-    const filterOptions: Record<string, any> = {};
-    if (filterField && filterValue) {
-      filterOptions[filterField as string] = filterValue;
-    }
+    const naturantsData = await features.query;
 
-    // Build the sorting options
-    const sortOptions: Record<string, SortOrder> = {};
-    if (sortField && sortOrder) {
-      sortOptions[sortField as string] = sortOrder as SortOrder;
-    }
-
-    // Build the field limiting options
-    const selectFields: string[] | undefined = Array.isArray(fields)
-      ? fields.map((field) => field.toString()) // Make sure each element is a string
-      : typeof fields === "string" // If it's a single string, convert it to an array
-      ? [fields]
-      : undefined;
-
-    // Convert page and limit to numbers (default to 1 and 10 respectively)
-    const pageNumber = page ? parseInt(page as string, 10) : 1;
-    const limitNumber = limit ? parseInt(limit as string, 10) : 100;
-
-    // Calculate the number of documents to skip
-    const skip = (pageNumber - 1) * limitNumber;
-
-    // Query the database with filtering, sorting, field limiting, and pagination options
-    let query = NaturantsModel.find(filterOptions)
-      .sort(sortOptions as any)
-      .skip(skip)
-      .limit(limitNumber);
-
-    // Apply field limiting if selectFields is defined
-    if (selectFields) {
-      query = query.select(selectFields);
-    }
-
-    // Execute the query
-    const naturantsData = await query.exec();
-
-    // Send the response
     res.json({
       status: "success",
       results: naturantsData.length,
@@ -81,6 +37,7 @@ export async function getAllNaturants(
     next(err);
   }
 }
+
 export async function createNaturant(
   req: express.Request,
   res: express.Response,
@@ -211,9 +168,8 @@ export async function getTopNaturants(
   next: express.NextFunction
 ) {
   try {
-    // Retrieve the top 5 restaurants (you might need to adjust the sorting logic)
     const topNaturantsData = await NaturantsModel.find()
-      .sort({ rating: -1 }) // Assuming you have a field named 'rating' to determine the top restaurants
+      .sort({ rating: -1 })
       .limit(5);
 
     res.json({
