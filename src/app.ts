c70483bc362a94be1930735d9733f3b7b651b3fe
Rowland Adimoha <rowlives@gmail.com>
2023-10-18 16:50:14 +0100

    add security paramters

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
+import mongoSanitize from "express-mongo-sanitize";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
+import hpp from "hpp";
 import morgan from "morgan";
 import { protect } from "./middleware/authMiddleware";
 import naturantsRoutes from "./routes/naturantsRoutes";
@@ -18,24 +20,30 @@ const port =
     ? process.env.PROD_PORT
     : process.env.DEV_PORT;
 
-// Middleware to set common response headers
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Content-Type", "application/json");
   next();
 });
 
-// Apply rate limiting middleware
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
   message: "Too many requests from this IP, please try again later.",
 });
 
 app.use(limiter);
 
 app.use(helmet());
-
 app.use(express.json());
+
+app.use(mongoSanitize());
+
+app.use(
+  hpp({
+    checkQuery: false,
+    checkBody: true,
+    whitelist: ["whitelistedParam"],
+  })
+);
 
 app.use(
   morgan(
@@ -45,21 +53,17 @@ app.use(
 
 app.use(cors());
 
-// Protect these routes
 app.use("/api/v1/users", usersRoutes);
 app.use("/api/v1/naturants", protect, naturantsRoutes);
 
-// Custom error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     res.status(err.statusCode).json({ error: err.message });
   } else {
     console.error("❌ Error:", err);
     if (process.env.NODE_ENV === "development") {
-      // In development, send detailed error information
       res.status(500).json({ error: err.message, stack: err.stack });
     } else {
-      // In production, send a generic error message
       res.status(500).json({ error: "Internal Server Error" });
     }
   }
