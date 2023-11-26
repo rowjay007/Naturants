ad1778005ac98546193c6cdc3f6d03028246c37e
Rowland Adimoha <rowlives@gmail.com>
2023-11-26 15:51:04 +0100

    fixing middleware build issues

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -2,16 +2,16 @@ import { Request, Response, NextFunction } from "express";
 
 // Custom interface extending the express Request interface
 interface CustomRequest extends Request {
-  userId?: number;
+  userId: string | number; // Excluding 'undefined' from the type
 }
 
 // Middleware to check if the 'id' parameter exists and is a valid number
 export function checkID(req: CustomRequest, res: Response, next: NextFunction) {
   const id = req.params.id;
-  if (id === undefined || isNaN(parseInt(id, 10))) {
+  if (id === undefined || isNaN(Number(id))) {
     return res.status(400).json({ error: "Invalid ID parameter" });
   }
-  req.userId = parseInt(id, 10); // Store the parsed ID in the request object
+  req.userId = Number(id); // Store the parsed ID in the request object
   next();
 }
 
